6d4fc60f7cc7973cd65e0f9239a882d786ba8752
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2020-05-22 22:46:38 +0200

    finish init the migrations

---
+++ hamzet_wasl_backend/database/migrations/2020_05_22_203120_create_workshops_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkshopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workshops', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('description');	
+            $table->dateTime('start_day',0);
+            $table->dateTime('end_date',0);
+            $table->integer('capcity');
+            $table->foreignId('admin_id')->references('id')->on('admins')->onDelete('cascade');
+            $table->foreignId('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workshops');
+    }
+}
