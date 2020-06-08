46f64901e8f10ba67bc73f593cd185ccef6e4c3e
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-06-08 18:33:40 +0200

    added the ability to add new workshop with design & alert when it is added

---
+++ hamzat-wasel-front-end/src/components/workshops/Add.js
@@ -0,0 +1,142 @@
+import React,{ Component} from 'react';
+import axios from 'axios';
+import config from '../token/token';
+import Cookies from 'universal-cookie';
+import AlertSuccess from '../categories/AlertSuccess';
+
+
+class Add extends Component {
+    constructor(props){
+        super(props)
+
+        const cookies = new Cookies();
+        const current_user = cookies.get('UserData');
+        console.log(current_user.id);
+        
+        this.state = {
+            title: '',
+            description: '',
+            start_date: '',
+            end_date: '',
+            capcity: '',
+            workshop_price: '',
+            user_id: current_user.id,
+            category_id: '1',
+            categories:[],
+            alert_message: '',
+        }
+    }
+
+    componentDidMount (){ 
+        axios.get('http://localhost:8000/sanctum/csrf-cookie').then(response => {
+            // console.log(response);
+            axios.get('http://localhost:8000/api/categories',config).then(res => {
+                console.log(res.data.data);
+                this.setState({ categories: res.data.data})
+                    
+            }).catch(error => {
+                console.log(error.response)
+            }); 
+        });
+    };
+
+    handleChange = ({target}) =>{
+        this.setState({ ...this.state, [target.name]: target.value });
+        console.log(target.name);
+        console.log(target.value);
+        console.log(this.state);   
+    };
+
+    onWorkshopAdded = e => {
+        e.preventDefault();  
+        axios.get('http://localhost:8000/sanctum/csrf-cookie').then(response => {
+            // console.log(response);
+            axios.post('http://localhost:8000/api/workshops',this.state,config).then(res => {
+                console.log(res.data);
+                
+                this.setState({alert_message: "success"});
+                setTimeout(() => this.setState({alert_message:''}), 7000);
+                
+            }).catch(error => { 
+                this.setState({alert_message: "error"});
+                setTimeout(() => this.setState({alert_message:''}), 7000);
+                console.log(error)
+            }); 
+        });
+    };
+
+
+render() {
+    console.log(this.state);  
+    return (     
+    <div className="container">
+        {this.state.alert_message === "success" ? 
+                <AlertSuccess message="Workshop is added successfully" /> : ""}
+        <div className="page-content">
+		    <div className="form-v7-content">
+                <form className="border border-success p-3 form-detail" onSubmit={this.onSubmit}>
+                    <div className="form-row ml-2">
+                        <label htmlFor="title" className="font-weight-bold mr-2">
+                            Name: </label>
+                        <input type="text" name="title" className="mr-2 input-text" 
+                            value={this.state.title} onChange={this.handleChange}/>
+                    </div> 
+                    <div className="form-row ml-2">
+                        <label htmlFor="workshops" className="font-weight-bold mr-2">
+                            Description: </label>
+                        <input type="text" name="description" className="mr-2 input-text" 
+                            value={this.state.description} onChange={this.handleChange}/>
+                    </div> 
+                    <div className="form-row ml-2">
+                        <label htmlFor="start_date" className="font-weight-bold mr-2">
+                            Start Date: </label>
+                        <input type="datetime-local" name="start_date" className="mr-2 input-text" 
+                            value={this.state.start_date} onChange={this.handleChange}/>
+                    </div> 
+                    <div className="form-row ml-2">
+                        <label htmlFor="end_date" className="font-weight-bold mr-2">
+                            End Date: </label>
+                        <input type="datetime-local" name="end_date" value={this.state.end_date}
+                            className="mr-2 input-text" onChange={this.handleChange}/>
+                    </div> 
+                    <div className="form-row ml-2">
+                        <label htmlFor="capcity" className="font-weight-bold mr-2">
+                            Number of attendees: </label>
+                        <input type="number" name="capcity" className="mr-2 input-text"
+                            value={this.state.capcity} onChange={this.handleChange}/>
+                    </div>
+                    <div className="form-row ml-2">
+                        <label htmlFor="workshop_price" className="font-weight-bold mr-2">
+                            Price: </label>
+                        <input type="number" name="workshop_price" className="mr-2 input-text" 
+                            value={this.state.workshop_price} onChange={this.handleChange}/>
+                    </div>
+                    <div className="form-row ml-2">
+                        <label htmlFor="workshop_price" className="font-weight-bold mr-2">
+                                Category: </label>
+                        <select name="category_id" className="form-control" style={{width:"200px"}} 
+                            onChange={this.handleChange}>
+                            {this.state.categories.map(category => {
+                            return (
+                                <option key={category.category_id} value={category.category_id}>
+                                    {category.category_name}
+                                </option>
+                            )
+                        })
+                        }
+                        </select>
+                    </div> 
+                    <div className="form-row-last">
+                        <input type="submit" onClick={this.onWorkshopAdded} 
+                            className="btn btn-success font-weight-bold" value="Add" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+        );
+    }
+}
+
+
+export default Add;
--- hamzat-wasel-front-end/src/components/workshops/Index.js
+++ hamzat-wasel-front-end/src/components/workshops/Index.js
@@ -1,6 +1,7 @@
 import React,{ Component} from 'react';
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 import AllWorkshops from './AllWorkshops';
+import Add from './Add';
 
 
 class Index extends Component {
@@ -12,8 +13,11 @@ render(){
             <div className="text-center">
                     <Link to="/workshops">
                         <button className="btn btn-primary m-3 p-2"> All Workshops</button></Link>
+                    <Link to="/workshops/add">
+                        <button className="btn btn-success p-2">Add Workshop</button></Link>
                 <Switch>
                         <Route exact path="/workshops" component={AllWorkshops} /> 
+                        <Route exact path="/workshops/add" component={Add} /> 
                 </Switch>
             </div>
         </Router>   
