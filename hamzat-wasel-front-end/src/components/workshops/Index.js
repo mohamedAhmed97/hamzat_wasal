9c2debc426e3a0617197e4842d1271ce7be6206d
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-06-13 14:51:21 +0200

    added each mentor to edit,delete his own workshops and only mentors can add workshops

--- hamzat-wasel-front-end/src/components/workshops/Index.js
+++ hamzat-wasel-front-end/src/components/workshops/Index.js
@@ -3,19 +3,24 @@ import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
 import AllWorkshops from './AllWorkshops';
 import Add from './Add';
 import Edit from './Edit';
+import Cookies from 'universal-cookie';
 
 
 class Index extends Component {
   
 render(){
+    const cookies = new Cookies();
+    const current_user=cookies.get('UserData');
     return(     
     <React.Fragment>
         <Router>
         <div className="text-center">
+            {(current_user && current_user.isAdmin == 1) ?
                     <Link to="/workshops">
-                        <button className="btn btn-primary mr-2 "> All Workshops</button></Link>
+                        <button className="btn btn-primary mr-2 "> All Workshops</button></Link> : "" }
+            {(current_user && current_user.isAdmin == 1) ?
                     <Link to="/workshops/add">
-                        <button className="btn btn-success">Add Workshop</button></Link>
+                        <button className="btn btn-success">Add Workshop</button></Link> : "" }
                     <Link to="/workshops" />
                 <Switch>
                         <Route exact path="/workshops" component={AllWorkshops} /> 
