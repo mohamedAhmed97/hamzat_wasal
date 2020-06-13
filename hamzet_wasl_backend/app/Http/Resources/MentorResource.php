6b07cf61119d7e692c932a1db3ac2d5e95cb6efe
Mayar Elabbasy <mayarelabbasy2016@gmail.com>
2020-06-13 14:48:57 +0200

    added id to Mentor Resource

--- hamzet_wasl_backend/app/Http/Resources/MentorResource.php
+++ hamzet_wasl_backend/app/Http/Resources/MentorResource.php
@@ -15,6 +15,7 @@ class MentorResource extends JsonResource
     public function toArray($request)
     {
         return [
+                'id' =>$this->id,
                 'name'=>$this->name,
                 'email'=>$this->email,
                 'avatar'=>$this->avatar,
